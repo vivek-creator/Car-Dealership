baae3ea12a742f47cbe04db21f26d690d0688b56
vivek-creator <tripathivivek898@gmail.com>
2024-04-23 23:36:38 +0530

    api changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -15,18 +15,19 @@ export const loginDetails = async (data)=> {
     return await axios.post(`${URL}/login`,data);
   } catch (error) {
     console.log('Error while calling login User API', error);
-  }
-}
-
-const handleBuy = async (car) => {
-  try {
-    // Stripe checkout code...
-
-    // After payment success
-    await transaction({ car, amount: car.price });
-    console.log("Transaction logged successfully!");
-
-  } catch (error) {
-    console.error('Network or server error:', error);
+    
   }
 };
+
+// const handleBuy = async (car) => {
+//   try {
+//     // Stripe checkout code...
+
+//     // After payment success
+//     await transaction({ car, amount: car.price });
+//     console.log("Transaction logged successfully!");
+
+//   } catch (error) {
+//     console.error('Network or server error:', error);
+//   }
+// };
